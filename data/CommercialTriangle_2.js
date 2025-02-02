852326d92fc4784979950135e0b03c9f4af7b70a
AccessLab <54842392+AccessLabTeam@users.noreply.github.com>
2025-02-02 12:07:34 +0200

    Add files via upload

---
+++ data/CommercialTriangle_2.js
@@ -0,0 +1,8 @@
+var json_CommercialTriangle_2 = {
+"type": "FeatureCollection",
+"name": "CommercialTriangle_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 23.728627577115237, 37.984237015384558 ], [ 23.735113189907334, 37.97552110918177 ], [ 23.725690563594686, 37.976144102911547 ], [ 23.728627577115237, 37.984237015384558 ] ] ] } }
+]
+}
---
+++ data/MetroStation_3.js
@@ -0,0 +1,10 @@
+var json_MetroStation_3 = {
+"type": "FeatureCollection",
+"name": "MetroStation_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Station": "MONASTIRAKI" }, "geometry": { "type": "Point", "coordinates": [ 23.725690563594686, 37.976144102911547 ] } },
+{ "type": "Feature", "properties": { "id": "2", "Station": "SYNTAGMA" }, "geometry": { "type": "Point", "coordinates": [ 23.735113189907334, 37.97552110918177 ] } },
+{ "type": "Feature", "properties": { "id": "3", "Station": "OMONOIA" }, "geometry": { "type": "Point", "coordinates": [ 23.728627577115237, 37.984237015384558 ] } }
+]
+}
